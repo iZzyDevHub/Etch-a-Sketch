3d6b1e6bc0f386eed3a939326cbe0d16a27cc1f3
Ishan Mithun <ishanmithun91@gmail.com>
2026-09-12 21:59:19 +0530

    All works before changes

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const container = document.getElementById('#container');
+const container = document.getElementById('container');
 
 for (let i = 0; i < 256; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
+    container.appendChild(square);
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'black';
+    });
+
     container.appendChild(square);
 }
