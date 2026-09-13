b5dc5398c283c40a64b2f4de559be9b17a7c8588
Ishan Mithun <ishanmithun91@gmail.com>
2026-09-13 19:22:03 +0530

    Do some changes

--- script.js
+++ script.js
@@ -9,4 +9,23 @@ function createGrid(squaresPerSide) {
     for (let i = 0; i < totalSquares; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
+
+        square.style.width = `${squaresize}px`;
+        square.style.height = `${squaresize}px`;
+
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'black';
+        });
+
+        container.appendChild(square);
+    }
+
 }
+
+const resetButton = document.querySelector('reset-button');
+resetButton.addEventListener('click', () => {
+    const squaresPerSide = prompt('Enter the number of squares per side (max 100): ');
+    
+})
+
+createGrid(16);
